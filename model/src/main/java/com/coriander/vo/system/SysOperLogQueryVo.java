a2e5ad5f72bb6bd1a8c46cb773cc141a5588f7f6
captain <260521812@qq.com>
2023-04-03 23:02:29 +0800

    搭建coriander-oa系统后端环境

---
+++ model/src/main/java/com/coriander/vo/system/SysOperLogQueryVo.java
@@ -0,0 +1,15 @@
+package com.coriander.vo.system;
+
+import lombok.Data;
+
+@Data
+public class SysOperLogQueryVo {
+
+	private String title;
+	private String operName;
+
+	private String createTimeBegin;
+	private String createTimeEnd;
+
+}
+
